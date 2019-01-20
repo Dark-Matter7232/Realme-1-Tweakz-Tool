36da3265084507eaf194f8db8de37881b5a7eec8
Your Name <you@example.com>
2019-01-20 17:58:00 +0530

    Upload The Files

---
+++ Readme.md
@@ -0,0 +1,37 @@
+//////--------//////-------////// //////--------//////-------////// //////--------//////-------////// //////--------//////-------////// //////--------//////
+Realme 1 Tweakz Tool is Created By AnuragRai 
+
+This Tools helps in removing coolpad Bloatware and replacing them with stock google apps and light apps like via browser,
+vlc player and retro player without Root and everytime 2.08 gb ram free(only 0.92 gb ram used)because bloatwares and ram eating core system apps
+removed :-)
+
+Steps to use this Tool
+1:go to setting->about phone
+2:click on build no. 7 times
+3:go to developer options
+4:click on usb debugging
+5:Open This Tool
+6:Select remove prloaded Apps(Option 1)
+7:Connect your phone
+8:press enter and Pop-up Will Apper
+9:Click on Allow Debugging
+10:wait for process to continue
+11:After process is done Select Option 2
+12:After process 2 is done Select Option 3
+13:Your device will reboot now and All bloatware will be gone(Launcher will be replaced with pixel Launcher)
+
+
+I have Created This Tool For Realme Because Rooting The Device Is Currently Not Possible 
+//////--------//////-------////// //////--------//////-------////// //////--------//////
+//////--------/////
+
+TTTTTTTTTTTTTTTTTTT    W           W   EEEEEEEEEEEEE   AAAAAAAAAAAAAA   K    K         /
+TTTTTTTTTTTTTTTTTTT    W           W   E               A            A   K   K         /
+        TT             W           W   E               A            A   K  K         /
+	    TT             W           W   E               A            A   K K         /
+		TT             W           W   E               A            A   KK         --------
+        TT             W     WW    W   EEEEEEEEEEEEE   AAAAAAAAAAAAAA   KK                /
+		TT             W    W  W   W   E               A            A   K  K             /
+		TT             W   W    W  W   E               A            A   K   K           /
+		TT             W  W      W W   E               A            A   K    K         /
+		TT             WW         WW   EEEEEEEEEEEEE   A            A   K     K       /
---
+++ Tweakz-Tool.bat
@@ -0,0 +1,98 @@
+@title Tweaks Tool For CPN5L
+@echo off
+adb\adb.exe start-server
+:OP0
+cls
+color 0e
+echo.
+echo  //////--------//////-------//////
+echo     Menu of Tweaks By AnuragRai 
+echo  --------------------------------------------------------------------------------------------------------------
+echo  To use this, Type the number of the option, and press the enter key. 
+echo --------------------------------------------------------------------------------------------------------------
+echo  1: Remove Preloaded apps.
+echo  2: Install Stock Google And Light Apps 
+echo  3: Reboot the Realme 1 "Do This After Step 1 and 2".
+echo  4: Remove Stock Launcher 
+echo --------------------------------------------------------------------------------------------------------------
+set Choice=
+set /p Choice=""
+
+if '%Choice%'=='1' goto OP1
+if '%Choice%'=='2' goto OP2
+if '%Choice%'=='3' goto OP3
+if '%Choice%'=='3' goto OP4
+	
+
+:OP1
+cls
+echo ---------------------------------------------------------------------------------------------------------------
+echo This will remove every Bloatware app exept for core apps and Settings And After This Step Use option 2 and 3
+echo Press any key to continu.
+echo ---------------------------------------------------------------------------------------------------------------
+pause >nul
+echo [*-*] Waiting for Device.
+adb\adb.exe wait-for-device
+adb\adb.exe uninstall -k --user 0 com.google.android.youtube
+adb\adb.exe uninstall -k --user 0 com.android.browser
+adb\adb.exe uninstall -k --user 0 com.coloros.cloud
+adb\adb.exe uninstall -k --user 0 com.google.android.youtube
+adb\adb.exe uninstall -k --user 0 com.google.android.music
+adb\adb.exe uninstall -k --user 0 com.google.android.videos
+adb\adb.exe uninstall -k --user 0 com.google.android.apps.photos
+adb\adb.exe uninstall -k --user 0 com.google.android.talk
+adb\adb.exe uninstall -k --user 0 com.google.android.apps.maps
+adb\adb.exe uninstall -k --user 0 com.google.android.apps.docs
+adb\adb.exe uninstall -k --user 0 com.google.android.apps.tachyon
+adb\adb.exe uninstall -k --user 0 com.oppo.market
+adb\adb.exe uninstall -k --user 0 com.coloros.oshare
+adb\adb.exe uninstall -k --user 0 com.emoji.keyboard.touchpal
+adb\adb.exe uninstall -k --user 0 com.android.chrome
+adb\adb.exe uninstall -k --user 0 com.coolpad.music
+adb\adb.exe uninstall -k --user 0 in.amazon.mShop.android.shopping
+adb\adb.exe uninstall -k --user 0 com.mediatek.mtklogger
+echo Done! Press any key to continue.
+pause >nul
+start Tweakz-Tool.bat
+exit
+
+:OP2
+echo.--------------------------------------------------------------------------------------
+echo This Will Install Lght Apps Instead Of those Ram Consuming ColorOS Apps.
+echo Press any key to continue.
+echo.--------------------------------------------------------------------------------------
+pause >nul
+echo [*-*] Waiting for Device
+adb\adb.exe wait-for-device
+echo [*-*] Installing Youtube Vanced app 1 of 6
+adb\adb.exe install apps/YoutubeVanced.apk
+echo [*-*] Installing MicroG For YT Vanced app 2 of 6
+adb\adb.exe install apps/MicroG.apk
+echo [*-*] Installing POCO Launcher app 3 of 6
+adb\adb.exe install apps/POCO.apk
+echo [*-*] Installing POCO Icon Pack 4 of 6
+adb\adb.exe install apps/ICON.apk
+echo [*-*] Installing Moded Download Manager app 5 of 6
+adb\adb.exe install apps/ADM.apk
+echo [*-*] Installing Via Browser 6 of 6
+adb\adb.exe install apps/VIA.APK
+echo [*-*] Done! Press any key to continue.
+pause >nul
+start Tweakz-Tool.bat
+exit
+
+:OP3
+echo [*-*] Waiting for Devce.
+adb\adb.exe wait-for-device
+adb\adb.exe reboot Device
+start Tweakz-Tool.bat
+exit
+
+:OP4
+echo Removing  Oppo Launcher(Make Surre That You Have Installed Another Launcher)
+echo [*-*] Waiting for Device.
+adb\adb.exe wait-for-device
+adb\adb.exe uninstall -k --user 0 com.oppo.Launcher
+start Tweakz-Tool.bat
+exit
+
